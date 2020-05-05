10c7272b3230fa01f5355d73df24881d044abbf6
Ali El-Sayed Ali <44211605+alisayed95@users.noreply.github.com>
2020-05-05 11:33:40 +0200

    Update SingleRecipe.js

--- src/Pages/SingleRecipe.js
+++ src/Pages/SingleRecipe.js
@@ -18,7 +18,7 @@ const SingleRecipe = (props) => {
     return ( 
         <div className='single-recipe'>
             <div className='col-1'>
-                <h2>{recipeData.recipe.label}</h2>
+                <h2>{recipeData.recipe.label.length < 35 ? recipeData.recipe.label : `${recipeData.recipe.label.substring(0,25)}...` }</h2>
                 <img src={recipeData.recipe.image} alt=""/>
                 <h3>Publisher : {recipeData.recipe.source}</h3>
                 <h3>For More Information <a href={recipeData.recipe.shareAs} target='_blank'  rel="noopener noreferrer" >Click Here</a></h3>   
@@ -27,7 +27,7 @@ const SingleRecipe = (props) => {
             <div className='col-2'>
                 <h2>{recipeData.recipe.ingredientLines.length} ingredients</h2>
                 <ul >
-                    {recipeData.recipe.ingredientLines.map((item,index) =>(<li key={index}>{item}</li>))}
+                    {recipeData.recipe.ingredientLines.map((item,index) =>(<li key={index}>{item.length < 20 ? item :`${item.substring(0,50)}...` }</li>))}
                 </ul>  
             </div>
         </div>
